eae9e9d9a6f58b797a87f82ffbca7a7bf3c98213
utsavbalar1231 <utsavbalar1231@gmail.com>
2025-08-15 23:21:32 +0530

    chore(eink): Add display_image_auto to SDK eink exports
    Change-Id: I90b9318a6d3467bab3f5e111d19a3215113613d1
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- src/distiller_cm5_sdk/hardware/eink/__init__.py
+++ src/distiller_cm5_sdk/hardware/eink/__init__.py
@@ -1,6 +1,6 @@
 from .display import (
     Display, DisplayError, DisplayMode, FirmwareType,
     ScalingMethod, DitheringMethod,
-    display_png, display_png_auto, clear_display, get_display_info,
+    display_png, display_png_auto, display_image_auto, clear_display, get_display_info,
     set_default_firmware, get_default_firmware, initialize_display_config
 )
